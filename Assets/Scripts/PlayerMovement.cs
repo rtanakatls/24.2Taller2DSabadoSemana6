1928e1aba72e747123b23721ab728c7d582f0354
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-09-28 10:55:51 -0500

    Added player behaviour

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        rb2d.velocity = new Vector2(horizontal, vertical).normalized * speed;
+    }
+}
